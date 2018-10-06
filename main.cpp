1edf81e28dafb7394639b447518c77731dcbf5d0
Omar Ahmad <eatmorerice@outlook.com>
2018-10-06 18:08:40 -0400

    Fixed main all by myself with no help

--- main.cpp
+++ main.cpp
@@ -20,12 +20,19 @@
 #include "SLL.hpp"
 
 using namespace std;
+int stars();
 
 void printGreatestBook(Book arr[], int length);// Part A main: Problem 1
 
 int main(){
-
-	// problem 1
+	int stars();
+	int stars();
+	int stars();
+	cout << " "<< endl;
+	cout << "Part A: Problem 1" << endl;
+	cout << " "<<endl;
+	cout<<"The info about the book with the greatest ratings: "<<endl;
+	cout<<" "<<endl;
 	int b1Ratings[10] = {6,5,8,7,2,8,2,9,4,10};
 	Book b1 = Book("Dan", "Brown", "The Da Vinci Code", 2003, b1Ratings);
 	int b2Ratings[10] = {7,3,7,1,6,2,6,1,6,3};
@@ -39,6 +46,43 @@ int main(){
 	Book bookArr[5] = {b1,b2,b3,b4,b5};
 	int length = 5;
 	printGreatestBook(bookArr, length);
+	stars();
+	stars();
+	stars();
+	cout<< " " <<endl;
+	cout<<"Part A: Problem 2:" <<endl;
+	cout<< " " <<endl;
+	int b1A2Ratings[10] = {6,8,3,8,3,2,7,3,4,6};
+	Book *b1A2 = new Book("Anna", "Sewell", "Black Beauty", 1877, b1A2Ratings);
+	b1A2->bookInfo();
+	cout<< " "<<endl;
+	stars();
+	stars();
+	stars();
+	cout<< " " <<endl;
+	cout<<"Part A: Problem 3:" <<endl;
+	cout<< " " <<endl;
+	int b1A3Ratings[10] = {6,8,3,8,3,2,7,3,4,6};
+	Book b1A3 = Book("Anna", "Sewell", "Black Beauty", 1877, b1A3Ratings);
+	int b2A3Ratings[10] = {6,2,7,6,1,9,10,5,3,8};
+	Book b2A3 = Book("Jack", "Higgins", "The Eagle Has Landed", 1975, b2A3Ratings);
+	Book *barr = new Book[2]{b1A3,b2A3};
+	cout<< "Printing out the details of book 1: "<<endl;
+	cout <<" "<<endl;
+	barr[0].bookInfo();
+	cout << " "<<endl;
+	cout<< "Printing the details of book 2: "<<endl;
+	cout<<" "<<endl;
+	barr[1].bookInfo();
+	cout<<" "<<endl;
+	stars();
+	stars();
+	stars();
+	cout<< " " <<endl;
+	cout<<"Part B: Problem 1:" <<endl;
+	cout<< " " <<endl;
+
+
 
 	return 0;
 }
@@ -54,3 +98,9 @@ void printGreatestBook(Book arr[], int length){ // putting length in the paramet
 	}
 	arr[position].bookInfo();
 }
+
+int stars(){ //prints out a line of stars to differentiate between different questions
+	cout << "******************************************************************************************************" << endl; // prints stars
+	return 0; //dummy return statement
+}
+
